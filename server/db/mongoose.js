ce2e9902ad05cbf52e3394da4f2b3d45be98866f
Aditya <adityasoni.iitr@gmail.com>
2018-02-27 00:47:20 +0530

    fixed bugs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise= global.Promise;
-mongoose.connect('mongodb://admin:admin@ds245478.mlab.com:45478/todo-api'|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGO_URI|| 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
